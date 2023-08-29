48dd7679d51fb3dfabe49dc08b8ac0a97bbc0ab4
Daniel <dwsuarez98@gmail.com>
2023-08-29 16:12:38 -0300

    doc: template listo, endpoint get x nombre

--- user_imc/urls.py
+++ user_imc/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import UsuariosCreateView, UsuariosRetrieveByNameView,UsuariosListView, UsuariosRetrieveByIDView, EstadisticasView
+from .views import UsuariosCreateView, UsuariosRetrieveByNameView,UsuariosListView, UsuariosRetrieveByIDView, EstadisticasView 
 
 urlpatterns = [
     path('api/registrar-usuario/', UsuariosCreateView.as_view(), name='registrar'),
     path('api/usuarios/<str:nombre>/', UsuariosRetrieveByNameView.as_view(), 
-         name='usuarios-retrieve-by-name'),
+         name='retrieve-by-name'),
     path('api/usuarios', UsuariosListView.as_view(), name='lista-registros'),
     path('api/usuarios/<int:id>/', UsuariosRetrieveByIDView.as_view(), name='usuario-id'),
     path('api/estadisticas/', EstadisticasView.as_view(), name='estadisticas'),
+    # Falta actualizar
 ]
--- user_imc/views.py
+++ user_imc/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView 
 from .serializer import UsuariosSerializer
@@ -50,3 +50,4 @@ class EstadisticasView(APIView):
         }
 
         return Response(estadisticas)
+
